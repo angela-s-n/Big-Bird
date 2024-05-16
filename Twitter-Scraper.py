bec64a40a7b39c0a81b4319c99ee2ae0b09a0301
angela-s-n <152636360+angela-s-n@users.noreply.github.com>
2024-05-16 18:03:51 +0100

    Update Twitter-Scraper.py
    use tweepy instead

--- Twitter-Scraper.py
+++ Twitter-Scraper.py
@@ -1,16 +1,23 @@
-import snscrape.modules.twitter as sntwitter
+import tweepy
+
+# Replace these values with your credentials
+consumer_key = 'your_consumer_key'
+consumer_secret = 'your_consumer_secret'
+access_token = 'your_access_token'
+access_token_secret = 'your_access_token_secret'
+
+# Authenticate to Twitter
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
 
 # Function to get tweet timestamps
-def get_tweet_timestamps(username, count=1000):
-    tweets = []
-    for i, tweet in enumerate(sntwitter.TwitterUserScraper(username).get_items()):
-        if i >= count:
-            break
-        tweets.append((tweet.id, tweet.date))
-    return tweets
+def get_tweet_timestamps(username, count=10):
+    tweets = api.user_timeline(screen_name=username, count=count, tweet_mode='extended')
+    timestamps = [(tweet.id_str, tweet.created_at) for tweet in tweets]
+    return timestamps
 
 # Example usage
-username = '0xBalloonLover'
-tweet_timestamps = get_tweet_timestamps(username, count=1000)
+username = 'twitter_username'
+tweet_timestamps = get_tweet_timestamps(username)
 for tweet_id, timestamp in tweet_timestamps:
     print(f'Tweet ID: {tweet_id}, Timestamp: {timestamp}')
